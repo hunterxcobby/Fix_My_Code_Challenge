321c67a89c87d826dd2809540a147bb644918a62
hunterxcobby <solomonsefah13@gmail.com>
2023-12-23 00:31:59 +0000

    :fix typp from FIzz to FIzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -20,7 +20,7 @@ def fizzbuzz(n):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            tmp_result.append("FIzz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
